0fa88169ff729c94e7aabd72e52a5321a4bdc64a
Your Name <e@w.com>
2022-09-07 16:49:35 +0300

    Сумма натуральный элементов между M и N

---
+++ Task_66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести числа от M до N. Найти сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N > M:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int Sum(int numM, int numN)//  ввод функции для рекурсии
+{
+    //Базовый случай
+    
+    if (numN-numM == 1) return numN+numM;
+    //Рекурсивный случай
+    return (numN+Sum(numM, numN-1));
+   
+}
+Console.WriteLine($"Сумма чисел от {numberM} до {numberN} = {Sum(numberM, numberN)}");
